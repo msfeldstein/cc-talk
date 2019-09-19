6df76f4ec9d523b12db03f3588d16dd2c659e96c
Michael Feldstein <msfeldstein@gmail.com>
2019-09-19 16:06:56 -0700

    Use cosines and sines

--- lines.js
+++ lines.js
@@ -15,23 +15,20 @@ const sketch = ({ context, width, height }) => {
     context.stroke();
   }
 
-  function drawBlobLine(x, time) {
-    for (var centerY = 0; centerY < height; centerY += 1) {
-      line(
-        x + Math.cos((centerY / height) * Math.PI * 4 + time / 2) * 200,
-        centerY,
-        x - Math.cos((centerY / height) * Math.PI * 4 + time) * 200,
-        centerY
-      );
-    }
-  }
-
   return ({ context, width, height, time }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
-    context.globalAlpha = 0.1;
-    for (var x = width / 2 - 600; x < width / 2 + 600; x += 50) {
-      drawBlobLine(x, time + ((x * 5232.325235) % 100));
+
+    context.translate(width / 2, height / 2);
+    for (var theta = 0; theta < 2 * Math.PI; theta += Math.PI / 60) {
+      var rStart = 30;
+      var rEnd = 300;
+
+      var x1 = Math.cos(theta) * rStart;
+      var y1 = Math.sin(theta) * rStart;
+      var x2 = Math.cos(theta) * rEnd;
+      var y2 = Math.sin(theta) * rEnd;
+      line(x1, y1, x2, y2);
     }
   };
 };
