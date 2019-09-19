05a79a63807c41892350b0285c3cd9f1b6267624
Michael Feldstein <msfeldstein@gmail.com>
2019-09-19 16:48:33 -0700

    Mix strokes and fill... kinda cool, but not that interesting really

--- lines.js
+++ lines.js
@@ -6,11 +6,17 @@ const settings = {
 };
 
 const sketch = ({ context, width, height }) => {
-  function circle(px, py, r, color) {
+  function circle(px, py, r, color, fill, opacity = 1) {
     context.beginPath();
     context.arc(px, py, r, 0, 2 * Math.PI);
-    context.strokeStyle = color;
-    context.stroke();
+    context.globalAlpha = opacity;
+    if (fill) {
+      context.fillStyle = color;
+      context.fill();
+    } else {
+      context.strokeStyle = color;
+      context.stroke();
+    }
   }
 
   return ({ context, width, height, time }) => {
@@ -18,7 +24,8 @@ const sketch = ({ context, width, height }) => {
     context.fillRect(0, 0, width, height);
     for (var x = 0; x < width; x += 100) {
       for (var y = 0; y < height; y += 100) {
-        circle(x, y, 50 + Math.sin(time + x + y) * 20, "#000000");
+        circle(x, y, 50 + Math.sin(time + x + y) * 20, "#000000", false, 0.3);
+        circle(x, y, 50 + Math.sin(-time + x + y) * 20, "#000000", true, 0.1);
       }
     }
   };
