8c9edee580b8cda588fd8375d99f420a3988a4d3
Michael Feldstein <msfeldstein@gmail.com>
2019-09-19 17:25:43 -0700

    Lets recreate the beesandbombs flower

--- lines.js
+++ lines.js
@@ -20,26 +20,8 @@ const sketch = ({ context, width, height }) => {
   }
 
   return ({ context, width, height, time }) => {
-    function eclipse(x, y, size, moonPhaseAmount) {
-      // Sun
-      context.globalCompositeOperation = "lighter";
-      circle(x, y, size, "#FF0000", true);
-      circle(x + Math.sin(time) * 5, y, size, "#00FF00", true);
-      circle(x + Math.sin(time * 1.2) * 5, y, size, "#0000FF", true);
-      // Moon
-      // circle(x + size * moonPhaseAmount, y, size * 0.99, "black", true);
-    }
-    context.fillStyle = "black";
+    context.fillStyle = "#FCD8D9";
     context.fillRect(0, 0, width, height);
-    const numMoons = 18;
-    for (var i = 0; i < numMoons; i++) {
-      eclipse(
-        width / 2 + Math.sin((i / numMoons) * 2 * Math.PI) * 350,
-        height / 2 + Math.cos((i / numMoons) * 2 * Math.PI) * 350,
-        80,
-        -1 + (i / numMoons) * 2 + Math.sin(i / 10 + time)
-      );
-    }
   };
 };
 
