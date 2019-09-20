e9e0865d6bc7504b6032aaee22780db300dd3e3a
Michael Feldstein <msfeldstein@gmail.com>
2019-09-19 17:44:24 -0700

    Animate around like in the gif

--- lines.js
+++ lines.js
@@ -23,24 +23,24 @@ const sketch = ({ context, width, height }) => {
     context.fillStyle = "#FCD8D9";
     context.fillRect(0, 0, width, height);
     context.translate(width / 2, height / 2);
-    function drawLeaf() {
+    function drawLeaf(pos) {
       var theta = -Math.PI / 2; // up
       var x = 0;
       var y = 0;
       var curlAmount = 0;
-      var amount = Math.cos(time) + 1;
+      var amount = Math.cos(time + pos * Math.PI * 2) + 1;
       for (var i = 0; i < 100; i++) {
         circle(x, y, 5 - (i / 100) * 5, "#003C28", true);
         x += Math.cos(theta) * 5;
         y += Math.sin(theta) * 5;
-        theta += curlAmount;
+        theta += (curlAmount * i) / 100;
         curlAmount += 0.01 * amount;
       }
     }
     var numLeafs = 12;
     for (var i = 0; i < numLeafs; i++) {
       context.rotate((Math.PI * 2) / numLeafs);
-      drawLeaf();
+      drawLeaf(i / numLeafs);
     }
   };
 };
